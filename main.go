1148cc56f583cc1e63112707e0e59db00375ff10
Ilyas vscode <ilyas.amantaev@gmail.com>
2022-11-30 21:52:29 +0600

    works

--- main.go
+++ main.go
@@ -1,103 +1,23 @@
-// package main
-
-// import (
-// 	"fmt"
-
-// 	"github.com/otiai10/gosseract/v2"
-// )
-
-// func main() {
-// 	client := gosseract.NewClient()
-// 	defer client.Close()
-// 	client.SetImage("data/ilyas-1.png")
-// 	text, _ := client.Text()
-// 	fmt.Println(text)
-// 	// Hello, World!
-// }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"log"
-
-// 	"code.sajari.com/docconv"
-// )
-
-// func main() {
-// 	res, err := docconv.ConvertPath("data/ilyas.pdf")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Println(res)
-// }
-
-// package main
-
-// import (
-// 	"bytes"
-// 	"fmt"
-// 	"log"
-
-// 	"github.com/ledongthuc/pdf"
-// )
-
-// func main() {
-// 	content, err := readPdf("data/ilyas.pdf") // Read local pdf file
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Println(content)
-// 	return
-// }
-
-// func readPdf(path string) (string, error) {
-// 	_, r, err := pdf.Open(path)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	totalPage := r.NumPage()
-
-// 	var textBuilder bytes.Buffer
-// 	for pageIndex := 1; pageIndex <= totalPage; pageIndex++ {
-// 		p := r.Page(pageIndex)
-// 		if p.V.IsNull() {
-// 			continue
-// 		}
-// 		text, err := p.GetPlainText("\n")
-// 		if err != nil {
-// 			log.Fatalln(err)
-// 		}
-// 		textBuilder.WriteString(text)
-// 	}
-// 	return textBuilder.String(), nil
-// }
-
 package main
 
 import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
-	"unicode"
+	"time"
 
 	"github.com/ledongthuc/pdf"
 	"github.com/xuri/excelize/v2"
 )
 
 var (
-	lg = log.New(os.Stderr, "ERROR:\t", log.Lshortfile)
+	lg        = log.New(os.Stderr, "ERROR:\t", log.Ltime)
+	checkDate = regexp.MustCompile(`\d\d\.\d\d.\d\d\d\d`)
 )
 
 func main() {
-	// for i := 0; i < 5; i++ {
-	// 	for j := 0; j < 3; j++ {
-	// 		fmt.Print(string([]rune{rune('A' + i)}) + strconv.Itoa((j + 1)))
-	// 	}
-	// 	fmt.Println()
-	// }
-
 	IDs, err := GetData("data")
 	if err != nil {
 		lg.Fatalln(err)
@@ -105,22 +25,19 @@ func main() {
 
 	f := excelize.NewFile()
 	index := f.NewSheet("Удостоверения")
-outerLoop:
-	for i := 0; i < len(IDs); i++ {
-		for j := 0; j < len(IDs[i]); j++ {
-			err := f.SetCellValue("Удостоверения", string([]rune{rune('A' + j)})+strconv.Itoa((i+1)), IDs[i][j])
-			if err != nil {
 
-				continue outerLoop
-			}
+	for i := 0; i < len(IDs); i++ {
+		for j := 0; j < len(IDs[i])-4; j++ {
+			// if i == 2 && len(IDs[i][j]) != 0 && unicode.IsDigit(rune(IDs[i][j][0])) {}
+			setRange := string([]rune{rune('A' + j)}) + strconv.Itoa((i + 1))
+			f.SetCellValue("Удостоверения", setRange, IDs[i][j])
 		}
 	}
 
 	f.SetActiveSheet(index)
-	_, err = os.ReadFile("Book1.xlsx")
-	if err == nil {
-		os.Remove("Book1.xlsx")
-	}
+	reNew("Book1.xlsx")
+
+	time.Sleep((1 * time.Second) / 2)
 	if err := f.SaveAs("Book1.xlsx"); err != nil {
 		fmt.Println(err)
 	}
@@ -132,25 +49,41 @@ func GetData(dirPath string) ([][]string, error) {
 	if err != nil {
 		return [][]string{}, err
 	}
+	i := 0
+	fmt.Println(" Тип |   Время  |          Имя файла          |  Статус  |")
 
-	for i, f := range files {
-		data = append(data, []string{})
+	for _, f := range files {
 
 		content, err := readPdf(dirPath + "/" + f.Name())
 		if err != nil {
-			// lg.Printf("File %s skipped: %s", f.Name(), err)
+			lg.Printf(" %-30s skipped", f.Name())
 			continue
 		}
-
+		if len(content) == 0 {
+			lg.Printf(" %-30s skipped", f.Name())
+			continue
+		}
+		data = append(data, []string{}, []string{})
 		data[i] = append(data[i], content...)
+		i++
 	}
 	return data, nil
+}
+
+func reNew(fileName string) {
+	_, err := os.ReadFile(fileName)
+	if err == nil {
+		os.Remove(fileName)
+	}
 }
 
 func readPdf(path string) ([]string, error) {
 	f, r, err := pdf.Open(path)
 	defer func() {
-		_ = f.Close()
+		err = f.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
 	}()
 	if err != nil {
 		return []string{}, err
@@ -163,17 +96,19 @@ func readPdf(path string) ([]string, error) {
 			continue
 		}
 
-		rows, _ := p.GetTextByRow()
+		rows, err := p.GetTextByRow()
+		if err != nil {
+			return []string{}, err
+		}
 
 		for i, row := range rows {
 			// println(">>>> row: ", row.Position)
 			for _, word := range row.Content {
-				if i == 2 && len(word.S) != 0 && unicode.IsDigit(rune(word.S[0])) {
-					data = append(data, "asodk")
-				} else {
-					data = append(data, word.S)
-				}
 
+				if i == 2 && checkDate.MatchString(word.S) {
+					data = append(data, "")
+				}
+				data = append(data, word.S)
 			}
 		}
 	}
--- regex.go
+++ regex.go
@@ -12,26 +12,15 @@ func cums() {
 	fmt.Println(match)
 
 	r, _ := regexp.Compile("p([a-z]+)ch")
-
 	fmt.Println(r.MatchString("peach"))
-
 	fmt.Println(r.FindString("peach punch"))
-
 	fmt.Println("idx:", r.FindStringIndex("peach punch"))
-
 	fmt.Println(r.FindStringSubmatch("peach punch"))
-
 	fmt.Println(r.FindStringSubmatchIndex("peach punch"))
-
 	fmt.Println(r.FindAllString("peach punch pinch", -1))
-
-	fmt.Println("all:", r.FindAllStringSubmatchIndex(
-		"peach punch pinch", -1))
-
+	fmt.Println("all:", r.FindAllStringSubmatchIndex("peach punch pinch", -1))
 	fmt.Println(r.FindAllString("peach punch pinch", 2))
-
 	fmt.Println(r.Match([]byte("peach")))
-
 	r = regexp.MustCompile("p([a-z]+)ch")
 	fmt.Println("regexp:", r)
 
